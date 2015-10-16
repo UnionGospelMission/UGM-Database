810411a44ddbab602c3cf668c2f52e07b59b6a01
lperkin1 <luke.perkins@uniongospelmission.org>
2015-10-15 17:43:01 -0700

    #39

--- custommiddleware/middleware.py
+++ custommiddleware/middleware.py
@@ -1,6 +1,13 @@
+from django.contrib.sessions.models import Session
+from django.contrib.auth.models import User
 
 class setBaseSite():
     def process_request(self,request):
+        print request.GET
+        print request.POST
+        print request.get_host()
+        uid = Session.objects.get(pk=request.session.session_key).get_decoded().get('_auth_user_id')
+        print User.objects.get(pk=uid)
         if request.path == '/':
             request.session['base_site']=''
         if not request.session.get('base_site',''):
