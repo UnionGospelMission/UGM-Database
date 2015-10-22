36e3e50e3fde166faa04b966e5e1a69c4290a6a8
lperkin1 <luke.perkins@uniongospelmission.org>
2015-10-22 13:18:38 -0700

    Removed password from post to logs

--- custommiddleware/middleware.py
+++ custommiddleware/middleware.py
@@ -1,10 +1,20 @@
 from django.contrib.sessions.models import Session
 from django.contrib.auth.models import User
 
+def interactiveConsole(a,b=None):
+    import code
+    d = {}
+    if b:
+        d.update(b)
+    d.update(a)
+    c=code.InteractiveConsole(locals=d)
+    c.interact()
+
+
 class setBaseSite():
     def process_request(self,request):
         print request.GET
-        print request.POST
+        print {k:v for k,v in request.POST.iteritems() if k!='password' and k!='csrfmiddlewaretoken'}
         print request.get_host()
         if request.session.session_key:
             uid = Session.objects.get(pk=request.session.session_key).get_decoded().get('_auth_user_id')
