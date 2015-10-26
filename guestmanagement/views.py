ebb43164d5dff477588e0825d70e091ffbe18a90
lperkin1 <luke.perkins@uniongospelmission.org>
2015-10-26 15:35:56 -0700

    Fixed if function issue

--- guestmanagement/views.py
+++ guestmanagement/views.py
@@ -294,6 +294,9 @@ class ReportProcessor():
         if operator == '=':
             if a==b:
                 self.listProcess(self.Env(env), deepcopy(code))
+            elif str(a).isdigit() and str(b).isdigit():
+                if int(str(a))==int(str(b)):
+                    self.listProcess(self.Env(env), deepcopy(code))
         elif operator == '>':
             if a>b:
                 self.listProcess(self.Env(env), deepcopy(code))
